45d26a5362fcfcc40aec3b3eaf71e2e59750361f
3STEB4N28 <71854065+3STEB4N28@users.noreply.github.com>
2021-03-28 22:04:00 -0400

    Update index.js
    Updated index.js

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ client.on('ready', () => {
     statcord.autopost();
 });
     statcord.on("autopost-start", () => {
-    // Emitted when statcord autopost starts
     console.log("Started autopost"); 
 });
 statcord.on("post", status => {
@@ -46,51 +45,7 @@ statcord.on("post", status => {
     else console.error(status);
 });
 
-//Callbacks, command handler and some commands
-
 bot.loadCommands("./commands/");
-
-bot.botJoinCommand({
-    channel: "794570132470366208", //the channel where <code> will be sent to
-    code: `$title[Bot Added]
-    $description[**Invite Link**
-    $getServerInvite
-    
-    **Name**
-    $serverName
-    
-    **Owner**
-    $usertag[$ownerID]
-   
-   **Members Count**
-   $membersCount
-    ]
-    $color[BLACK]
-
-$djsEval[const fs = require('fs');
-
-fs.access('/home/container/db/warnings/$guildID.json', (err) => {
-    if (err) {
-       fs.writeFile("/home/container/db/warnings/$guildID.json", '{}', function(err) {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log("New db successfully created for $guildID")
-  }
-})
-    } else {
-        console.log("$guildID is already registered.");
-    }
-});]
-    ` //message sent to <channel>
-});
-//bot.onGuildJoin(); //callback itself
-
-bot.botLeaveCommand({//command
-    channel: "794570415023849473",//the channel where <code> will be sent to
-    code: `Bot removed from $serverName`//message sent to <channel>
-});
-//bot.onGuildLeave(); //callback itself
 
 /*bot.readyCommand({
     channel: "802775876420894780",
@@ -100,77 +55,9 @@ bot.botLeaveCommand({//command
     Users: $numberSeparator[$allMembersCount]
     ]
     $color[BLACK]`
-});
-
-bot.deletedCommand({
-    channel: "$channelID",
-    code: `$setChannelVar[deletedMessage;$message;$channelID]
-    $setChannelVar[deletedMessageAuthor;$authorID;$channelID]
-    $setChannelVar[deletedMessageAuthorIcon;$authorAvatar;$channelID]
-    $setChannelVar[deletedMessageDate;$month-$day-$year, $hour:$minute:$second;$channelID]
-    `
-});
-bot.onMessageDelete();
-
-bot.updateCommand({
-    channel: "$channelID",
-    code: `$setChannelVar[beforeEditedMessage;$oldMessage;$channelID]
-    $setChannelVar[editedMessage;$message;$channelID]
-    $setChannelVar[editedMessageAuthor;$authorID;$channelID]
-    $setChannelVar[editedMessageAuthorIcon;$authorAvatar;$channelID]
-    $setChannelVar[editedMessageDate;$month-$day-$year, $hour:$minute:$second;$channelID]`
-});
-bot.onMessageUpdate();
-
-bot.command({
-    name: "snipe",
-    aliases: ['s'],
-    description: "See the recently deleted message!",
-    usage: "snipe",
-    permission: "MANAGE_MESSAGES",
-    code: `
-    $author[$userTag[$getChannelVar[deletedMessageAuthor]]]
-    $description[$getChannelVar[deletedMessage]]
-   $thumbnail[$getChannelVar[deletedMessageAuthorIcon]]
-    $color[BLACK] 
-  $footer[$getChannelVar[deletedMessageDate]]
-    
-    $channelSendMessage[$getVar[cmdslogs];$username[$authorID]#$discriminator[$authorID] Used command \`snipe\` in server: $serverName[$guildID]]
-
-$setVar[cmdsused;$sum[$getVar[cmdsused];1]]
-    
-    $onlyIf[$getGlobalUserVar[blacklisted;$authorID]!=true;You’re blacklisted from using the bot!]
- $onlyIf[$getServerVar[blacklisteds]!=true;This guild is blacklisted from using the bot!]`
-});
-
-bot.command({
-    name: "esnipe",
-    aliases: ['es'],
-    description: "See the recently edited message!",
-   usage: "esnipe",
-   permission: "MANAGE_MESSAGES",
-    code: `$author[$userTag[$getChannelVar[editedMessageAuthor]]]
-    $description[$addField[After;\`$getChannelVar[editedMessage]\`]
-    $addField[Before;\`$getChannelVar[beforeEditedMessage]\`]]
-    $thumbnail[$getChannelVar[editedMessageAuthorIcon]]
-    $footer[$getChannelVar[editedMessageDate]]
-    $color[BLACK]
-    $onlyIf[$hasPerms[$authorID;managemessages]=true;You need the \`MANAGE_MESSAGES\` permission!]
-   $onlyIf[$getGlobalUserVar[blacklisted;$authorID]!=true;You’re blacklisted from using the bot!]
-
-$onlyIf[$getServerVar[blacklisteds]!=true;This guild is blacklisted from using the bot!]
-
-$channelSendMessage[$getVar[cmdslogs];$username[$authorID]#$discriminator[$authorID] Used command \`esnipe\` in server: $serverName[$guildID]]
-
-$setVar[cmdsused;$sum[$getVar[cmdsused];1]]`
 });*/
-/*bot.onJoined();
-bot.onLeave();*/
-
-//Variables
 
 bot.variables({
-//settings
     prefix: "h!",
     myid: "701292425624420362",
     moderation: "on",
@@ -178,24 +65,17 @@ bot.variables({
     fun: "on",
     music: "on",
     welcomer: "on",
-//Economy
     Money: 0,
     Bank: 0,
-//Upvote/downvote system
     upvote: 0,
     downvote: 0,
-//afk system
     afk: "off",
     afkmsg: "None",
-//idk
     premium: false,
-//Systems
     cmdsused: 22103,
     cmdslogs: "789270287501492275",
-    lang: "en",
     blacklisted: false,
     blacklisteds: false,
-//Welcomer
     joinmsg: "Not settled",
     leavemsg: "Not settled",
     joinc: "Not settled",
@@ -205,14 +85,11 @@ bot.variables({
     leavecolor: "RANDOM",
     joinfooter: "Welcome!",
     leavefooter: "Hopefully they come back soon!",
-//Warnings system
     warn: 0,
-//Snipe system
     deletedMessage: "",
     deletedMessageAuthor: "",
     deletedMessageAuthorIcon: "",
     deletedMessageDate: "",
-//Editsnipe system
     editedMessage: "",
     beforeEditedMessage: "",
     editedMessageAuthor: "",
@@ -220,17 +97,10 @@ bot.variables({
     editedMessageDate: "",
     editedMessageLink: "",
 });
-//Statuses
 
 bot.status({
       text: "$numberSeparator[$serverCount;,] Servers! | $numberSeparator[$allMembersCount] Users!",
       type: "WATCHING",
       time: 10
-});
-
-bot.status({
-    text: "Expected Downtime!",
-    type: "PLAYING",
-    time: 15
 });
 client.login(config.token)
