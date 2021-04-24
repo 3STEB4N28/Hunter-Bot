ad4d68c895478b69792b9ad2301e9af0d120ecd6
3STEB4N28 <71854065+3STEB4N28@users.noreply.github.com>
2021-04-24 14:46:48 -0400

    Update and rename index 2.js to index.js

--- index.js
+++ index.js
@@ -24,23 +24,13 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-app.get("/test", async (req, res) => {
-    res.send(`Current ${client.user.tag}'s servers: ${client.guilds.cache.size}`);
-    console.log(`Endpoint /test was requested by someone!`)
-});
-
-app.get("/status", (req, res) => {
-    res.sendStatus(200);
-});
-
 app.post("/votes", function (request, response) {
-    const webhook1 = new Discord.WebhookClient("798417003727028255", "ml00TMipm_vwda3lvHbNlomdcYTkztk3zeAA0y60XJTTC7bJb7AwC3bJT9glr1zKdTsl");
-    const webhook2 = new Discord.WebhookClient("832879832370774046","ildScDou1jnNnF2Xgr0AbjqLp1fsHF99QDKHkMSJSn9Y-hXQBWFTfXk333xzFlAgG9Ds"); 
+    const webhook1 = new Discord.WebhookClient("public-webhook-id", "public-webhook-token");
+    const webhook2 = new Discord.WebhookClient("private-webhook-id","private-webhook-token"); 
     let auth = request.headers.authorization
-   // console.log(request.body)
     let req = request.body
-  response.sendStatus(200) // tells the website that sent the POST request that everything was OK
-  if (auth === "topgg1") {
+  response.sendStatus(200) // Tells the website that sent the POST request that everything was OK
+  if (auth === "top.gg") {
       console.log(`[TOP.GG] ${req.user} just voted for ${req.bot} is weekend: ${req.isWeekend}`)
       let embed = new Discord.MessageEmbed()
       .setTitle(`Vote Received`)
@@ -54,12 +44,12 @@ This helps me grow and get more user's to know me!`)
       webhook2.send(`${req.user} top.gg ${req.isWeekend}`)
   } 
   
-  else if (auth === "ibl1") {
+  else if (auth === "infinitybotlist") {
       console.log(`[IBL] ${req.userID} just voted for ${req.botID}`) //userID
       let embed = new Discord.MessageEmbed()
       .setTitle(`Vote Received`)
       .setURL(`https://infinitybotlist.com/bots/${req.bot}`)
-      .setDescription(`<@!${req.userID}> Thanks for voting me **Infinity Bot List**! 
+      .setDescription(`<@!${req.userID}> Thanks for voting me on **Infinity Bot List**! 
       
 This helps me grow and get more user's to know me!`)
       .setColor("BLACK")
@@ -68,7 +58,7 @@ This helps me grow and get more user's to know me!`)
       webhook2.send(`${req.userID} ibl`)
   } 
   
-  else if (auth === "dl1") {
+  else if (auth === "discordlabs") {
       console.log(`[DL] ${req.uid} voted for ${req.bid}`)
       let embed = new Discord.MessageEmbed()
       .setTitle(`Vote Received`)
@@ -82,7 +72,7 @@ This helps me grow and get more user's to know me!`)
       webhook2.send(`${req.uid} dl`)
   }
   
-   else if (auth === "tc1") {
+   else if (auth === "topcord") {
       console.log(`[TC] ${req.User.ClientID} voted for ${req.ClientID}`)
       let embed = new Discord.MessageEmbed()
       .setTitle(`Vote Received`)
@@ -96,7 +86,7 @@ This helps me grow and get more user's to know me!`)
       webhook2.send(`${req.User.ClientID} tc`)
   }
   
-   else if (auth === "abl1") {
+   else if (auth === "astrobotlist") {
       console.log(`[ABL] ${req.voted} just voted for ${req.bot} total votes: ${req.totalVotes}`)    
       let embed = new Discord.MessageEmbed()
       .setTitle(`Vote Received`)
@@ -110,7 +100,7 @@ This helps me grow and get more user's to know me!`)
       webhook2.send(`${req.voted} abl`)
   }
   
-   else if (auth === "bbl1") {
+   else if (auth === "bladebotlist") {
       console.log(`[BBL] ${req.userid} voted for ${req.botID}`) 
       let embed = new Discord.MessageEmbed()
       .setTitle(`Vote Received`)
@@ -124,7 +114,7 @@ This helps me grow and get more user's to know me!`)
       webhook2.send(`${req.userid} bbl`)
   }
   
-   else if (auth === "bfd1") {
+   else if (auth === "botsfordiscord") {
       console.log(`[BFD] ${req.user} voted for ${req.bot}`)
       let embed = new Discord.MessageEmbed()
       .setTitle(`Vote Received`)
@@ -138,7 +128,7 @@ This helps me grow and get more user's to know me!`)
       webhook2.send(`${req.user} bfd`)
   }
   
-   else if (auth === "dbl1") {
+   else if (auth === "discordbotlist") {
       console.log(`[DBL] ${req.id} voted for ${client.user.id}`)
       let embed = new Discord.MessageEmbed()
       .setTitle(`Vote Received`)
@@ -152,11 +142,11 @@ This helps me grow and get more user's to know me!`)
       webhook2.send(`${req.id} dbl`)
   }
 
-   else if (auth === "dboats1") {
+   else if (auth === "discordboats") {
        console.log(`[DBOATS] ${req.user.id} voted for ${req.bot.id}`)
    }
   
-  else if (auth === "vb1") {
+  else if (auth === "voidbots") {
       console.log(`[VB] ${req.user} voted for ${req.bot}`) 
       let embed = new Discord.MessageEmbed()
       .setTitle(`Vote Received`)
@@ -193,29 +183,30 @@ app.listen(2000, () => {
     console.log("Website + Webhook is up and running");
 });
 
-let id = "755094850113896639"
+let id = "your-bots-client-id-here"
 
 client.on('ready', () => {
     statcord.autopost();
     const blapi = require("blapi");
     const apiKeys = {
         // Add other bot lists as needed
- 		"botlist.space": config.blspace,
-  		"bladebotlist.xyz": config.bladebl,
-  		"blist.xyz": config.blist,
-  		"botlists.com": config.botlists,
-  		"botsfordiscord.com": config.bfd,
-  		"discord-botlist.eu": config.dbleu,
-  		"discordbotlist.com": config.dblist,  					
-        "discordlistology.com": config.dlo,  					
-        "infinitybotlist.com": config.ibl,
-  		"voidbots.net": config.voidb,
-  		"space-bot-list.xyz": config.sbl, 
-        "arcane-center.xyz": config.ac,  						
-  		"discordextremelist.xyz": config.del,
-  		"botrix.cc": config.btr,
-        "fateslist.xyz": config.fateslist,
-        "botlists.com": config.abl,
+	    "top.gg": config.topgg,
+ 	    "botlist.space": config.blspace,
+            "bladebotlist.xyz": config.bladebl,
+            "blist.xyz": config.blist,
+  	    "botlists.com": config.botlists,
+  	    "botsfordiscord.com": config.bfd,
+            "discord-botlist.eu": config.dbleu,
+  	    "discordbotlist.com": config.dblist,  					
+	    "discordlistology.com": config.dlo,  					
+	    "infinitybotlist.com": config.ibl,
+  	    "voidbots.net": config.voidb,
+  	    "space-bot-list.xyz": config.sbl, 
+            "arcane-center.xyz": config.ac,  						
+  	    "discordextremelist.xyz": config.del,
+  	    "botrix.cc": config.btr,
+            "fateslist.xyz": config.fateslist,
+            "botlists.com": config.abl,
 
 };
 blapi.handle(client, apiKeys, 90);
@@ -260,10 +251,7 @@ bot.variables({
 	miscstatus: "on",
 	modstatus: "on",
 	welcstatus: "on",
-	logsst: "off",
-	logswebhooktoken: "oE2BqaooBJ_PAb3A3NgLcZExRQK4-kEmJoL1LadDeZ-HlxSOVKqro9WmAe8BSHsfXt5l",
-	logswebhookid: "830868134210306079",
-	ecologs: "829454196771520582",
+	ecologs: "Economy logs channel id here",
     logschannel: "",
 	botfooter: "©2021 Hunter Bot Development Team",
 	
@@ -273,37 +261,16 @@ bot.variables({
 	jobmulti: 1,
 	whrs: 0,
 	ecosign: "🔹",
-    teamscount: 0,
-	teamBank: 0,
-	teamName: "",
-	teamMembers: "",
-	teamInvite: false,
-	teamHP: 100,
-	teamLevel: 0,
-	teamXP: 0,
-	teamXPReq: 100,
-	teamOwner: "",
-	teamCoOwner: "",
-	hourlyBox: 0,
-	dailyBox: 0,
-	weeklyBox: 0,
-	monthlyBox: 0,
-	hourlyCase: 0,
-	dailyCase: 0,
-	weeklyCase: 0,
-	monthlyCase: 0,
-	votePoints: 0,
-	voteBox: 0,
-	
+    	
 	botlastmsgid: "",
 	upvote: 0,
 	downvote: 0,
 	upvotes: 0,
 	downvotes: 0,
 	cmdsused: 22172,
-    statsmsg: "831910034215796746",
-    usercmdsused: 0,
-	cmdslogs: "829454222067236884",
+        statsmsg: "a status messae id that the bot will edit with the current statistics of it",
+        usercmdsused: 0,
+	cmdslogs: "the commands logs channel",
 	blacklisted: false,
 	blacklisteds: false,
 	blacklistedreason: "",
@@ -336,51 +303,23 @@ bot.variables({
 });
 
 bot.status({
-      text: "$numberSeparator[$serverCount;,]/250 Servers! | $numberSeparator[$allMembersCount] Users!",
+      text: "$numberSeparator[$serverCount;,] Servers! | $numberSeparator[$allMembersCount] Users!",
       type: "WATCHING",
       time: 10
 });
 
-/*bot.status({
-    text: "V4.0.0 This weekend!",
-    type: "PLAYING",
-    time: 15
-});
-
-bot.status({
-    text: "Development mode enabled!",
-    type: "WATCHING",
-    time: 15
-});*/
-
 bot.loopCommand({
-	channel: "829454380373114950",
-	code: `$editChannel[829454380373114950;$default;Servers Count: $numberSeparator[$serverCount;,];$default;$default;$default;default]`,
-	executeOnStartup: true,
-	every: 120000 //2 minutes
-});
-bot.loopCommand({
-	channel: "830618658975514676",
-	code: `$editChannel[830618658975514676;$default;Users Count: $numberSeparator[$allMembersCount;,];$default;$default;$default;$default]`,
-	executeOnStartup: true,
-	every: 120000,
-});
-bot.loopCommand({
-    channel: "$findChannel[stats]",
+    channel: "status channel id"
     code: `$log[Statistics updated on Hunter's Lounge!]
     $editMessage[$getVar[statsmsg];{title:$userTag[$clientid]'s Statistics}
     {description:Current statistics of the bot!}
     {field:Current Version:\`v4.0.0\`:no}
     {field:Next Version: \`v5.0.0\`:no}
     {field:Current Language:\`DBD.js-lite v$packageVersion\`:no}
-    {field:Next Language:\`Discord.js\`:no}
     {field:Total Servers:\`$numberSeparator[$serverCount]\`:no}
     {field:Total Users:\`$numberSeparator[$allMembersCount]\`:no}
     {field:Total Channels:\`$numberSeparator[$allChannelsCount]\`:no}
     {field:Total Commands Used:\`$numberSeparator[$getVar[cmdsused]]\`:no}
-    {field:Total Blacklisted Users:\`$numberSeparator[$getVar[blacklistedUsers]]\`:no}
-    {field:Total Blacklisted Servers:\`$numberSeparator[$getVar[blacklistedServers]]\`:no}
-    {field:Total Teams:\`$numberSeparator[$getVar[teamscount]]\`:no}
     {color:BLACK}
     {footer:Last updated at $month $dayth $year $hour-$minute EST}
     $timezone[America/New_York]
